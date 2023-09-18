4f64e8108b94a98b5c2645f2b621380f2675bb8e
CN-Curriculum <138328232+CN-Curriculum@users.noreply.github.com>
2023-09-18 11:25:17 -0400

    Updated magicalCreature coding exercise

---
+++ magicalCreature.js
@@ -0,0 +1,51 @@
+// 1. Create a variable named `place` and set the value to a string that says whatever place you want.
+
+// 2. Create a variable named `creatureName` and set the value to a string that says whatever name you want to give the creature.
+
+// 3. Create a variable named `color` and set the value to a string that says whatever color you want.
+
+// 4. Create a variable named `color2` and set the value to a string that says a different color you want.
+
+// 5. Create a variable named `emotion` and set the value to a string that says whatever emotion you want.
+
+// 6. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
+
+// 7. Create a variable named `character` and set the value to a string that says whatever name for a character you want.
+
+// 8. Create a variable named `number` and set the value to a number greater than 1.
+
+// 9. Create a variable named `item` and set the value to a string that says whatever item/thing you want.
+
+// 10. Create a variable named `item2` and set the value to a string that says a different item/thing you want.
+
+// 11. Create a variable named `verb` and set the value to a string that says whatever verb you want.
+
+// 12. Create a variable named `adverb` and set the value to a string that says whatever adverb you want.
+
+// 13. Create a variable named `number2` and set the value to a number greater than 1.
+
+
+
+
+
+
+// 14. Replace the null values with the appropriate variable you created above.
+// - Afterwards, run your code and press the button to see your story ✨
+
+document.querySelector("button").addEventListener("click", function() {
+    document.querySelector(".place").innerHTML = null;
+    document.querySelector(".creature-name").innerHTML = null;
+    document.querySelector(".color").innerHTML = null;
+    document.querySelector(".color-2").innerHTML = null;
+    document.querySelector(".emotion").innerHTML = null;
+    document.querySelector(".character").innerHTML = null;
+    document.querySelector(".creature-name-again").innerHTML = null;
+    document.querySelector(".number").innerHTML = null;
+    document.querySelector(".item").innerHTML = null;
+    document.querySelector(".character-again").innerHTML = null;
+    document.querySelector(".item-2").innerHTML = null;
+    document.querySelector(".verb").innerHTML = null;
+    document.querySelector(".adverb").innerHTML = null;
+    document.querySelector(".number-2").innerHTML = null;
+    document.querySelector(".story-container").style = "display:block";
+});
---
+++ variables.js
@@ -0,0 +1,18 @@
+// CODE ALONG, PART 1
+let favoriteColor;
+
+// 1. Create a variable called `favoriteFood`.
+
+
+// 2. Create a variable called `favoriteNumber`.
+
+
+
+
+// CODE ALONG, PART 2
+//  3. Assign values for each variables.
+// - Use a string value of your favorite color.
+// - Use a string value of your favorite food.
+// - Use a number value of your favorite number.
+
+// 4. Console log each variable on a new line.
